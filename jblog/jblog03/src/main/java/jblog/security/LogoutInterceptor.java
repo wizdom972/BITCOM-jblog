82ca4e3aca405ee38c1a0400c236829f5495358e
wizdom972 <jihae0584@gmail.com>
2025-01-14 16:14:08 +0900

    ✨ 로그아웃 인터셉터 구현

--- jblog/jblog03/src/main/java/jblog/security/LogoutInterceptor.java
+++ jblog/jblog03/src/main/java/jblog/security/LogoutInterceptor.java
@@ -2,6 +2,25 @@ package jblog.security;
 
 import org.springframework.web.servlet.HandlerInterceptor;
 
-public class LogoutInterceptor implements HandlerInterceptor {
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
+public class LogoutInterceptor implements HandlerInterceptor {
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		HttpSession session = request.getSession();
+		
+		if (session != null) {
+			session.removeAttribute("authUser");
+			session.invalidate();
+		}
+
+		response.sendRedirect(request.getContextPath());
+
+		return false;
+	}
+	
 }
--- jblog/jblog03/src/main/webapp/WEB-INF/views/main/index.jsp
+++ jblog/jblog03/src/main/webapp/WEB-INF/views/main/index.jsp
@@ -13,9 +13,9 @@
 	<div class="center-content">
 		<h1 class="logo">JBlog</h1>
 		<ul class="menu">
-			<li><a href="${pageContext.request.contextPath}/user">로그인</a></li>
+			<li><a href="${pageContext.request.contextPath}/user/login">로그인</a></li>
 			<li><a href="${pageContext.request.contextPath}/user/join">회원가입</a></li>
-			<li><a href="${pageContext.request.contextPath}/user">로그아웃</a></li>
+			<li><a href="${pageContext.request.contextPath}/user/logout">로그아웃</a></li>
 			<li><a href="${pageContext.request.contextPath}">내블로그</a></li>
 		</ul>
 		<form class="search-form">
